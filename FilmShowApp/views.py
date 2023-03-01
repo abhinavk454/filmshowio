832abacc72a9adcf691c2af2a45d0bb0b747af18
Abhinav Kumar <abhinav.vikash2@gmail.com>
2023-03-01 19:16:40 +0530

    Increased timeout

--- FilmShowApp/views.py
+++ FilmShowApp/views.py
@@ -2,27 +2,11 @@ from .models import Video
 from .serializers import VideoSerializers
 from rest_framework import generics
 from django.http import StreamingHttpResponse
-from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
 
 
 class UploadVideo(generics.CreateAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializers
-    parser_classes = (MultiPartParser, FormParser)
-
-    def post(self, request, *args, **kwargs):
-        file_obj = request.FILES['file']
-        chunk_size = 1024 * 1024  # 1MB
-        video = Video()
-        video.file.save(file_obj.name, file_obj)
-        with video.file.open(mode='ab') as destination:
-            for chunk in file_obj.chunks(chunk_size):
-                destination.write(chunk)
-        video.save()
-        serializer = VideoSerializers(video)
-        return Response(serializer.data, status=HTTP_201_CREATED)
 
 
 class GetVideo(generics.ListAPIView):
@@ -48,6 +32,6 @@ class GetStreamingVideo(generics.RetrieveAPIView):
                     yield chunk
 
         response = StreamingHttpResponse(stream_file(video_file))
-        response['Content-Type'] = 'video/mp4'
+        response['Content-Type'] = 'video/*'
         response['Content-Length'] = video_file.size
         return response
