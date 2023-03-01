6235986f0565f311b0783ad96c4577125337a5ce
Abhinav Kumar <abhinav.vikash2@gmail.com>
2023-03-01 11:18:45 +0530

    Added Streaming Endpoint

--- FilmShowApp/urls.py
+++ FilmShowApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UploadVideo, GetVideo
+from .views import UploadVideo, GetVideo, GetStreamingVideo
 
 urlpatterns = [
     path("upload/", UploadVideo.as_view()),
     path("list/", GetVideo.as_view()),
+    path("videos/<int:id>/stream/",
+         GetStreamingVideo.as_view(), name='video-stream'),
 ]
--- FilmShowApp/views.py
+++ FilmShowApp/views.py
@@ -1,6 +1,7 @@
 from .models import Video
 from .serializers import VideoSerializers
 from rest_framework import generics
+from django.http import StreamingHttpResponse
 
 
 class UploadVideo(generics.CreateAPIView):
@@ -11,3 +12,26 @@ class UploadVideo(generics.CreateAPIView):
 class GetVideo(generics.ListAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializers
+
+
+class GetStreamingVideo(generics.RetrieveAPIView):
+    queryset = Video.objects.all()
+    lookup_field = 'id'
+    serializer_class = VideoSerializers
+
+    def get(self, request, *args, **kwargs):
+        video = self.get_object()
+        video_file = video.file
+
+        def stream_file(file):
+            with open(file.path, 'rb') as f:
+                while True:
+                    chunk = f.read(1024)
+                    if not chunk:
+                        break
+                    yield chunk
+
+        response = StreamingHttpResponse(stream_file(video_file))
+        response['Content-Type'] = 'video/mp4'
+        response['Content-Length'] = video_file.size
+        return response
